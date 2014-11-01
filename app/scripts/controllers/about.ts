8670357e1b55d745eec17093a8a8417d904028e4
Somik Raha <somikr@gmail.com>
2014-11-01 15:50:12 -0700

    Added required controller files

---
+++ app/scripts/controllers/about.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scriptureSearchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scriptureSearchApp
+ */
+angular.module('scriptureSearchApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scriptureSearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scriptureSearchApp
+ */
+angular.module('scriptureSearchApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
