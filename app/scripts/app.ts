b7ca9d60bf1c7dd20a16f3bee39c1da7cac3f0a9
Somik Raha <somikr@gmail.com>
2014-11-01 15:48:27 -0700

    Added app.ts

---
+++ app/scripts/app.ts
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scriptureSearchApp
+ * @description
+ * # scriptureSearchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scriptureSearchApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
